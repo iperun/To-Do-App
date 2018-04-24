f79a42b6f9334b2e96ec61115eaa96ccd555f8eb
Ivan Perun <peruni101@gmail.com>
2018-04-24 14:33:15 -0400

    js button add/delete items

---
+++ js/main.js
@@ -0,0 +1,21 @@
+function addItem() {
+  if ($('input:text').val().length == 0) {
+    $('#todo').effect("shake", {
+      distance: 5,
+      times: 2
+    }, 400);
+  } else {
+    var text = $('#new-todo').val();
+    $('#to-do-list').append('<li><input type="checkbox" class="done" required/>' + text + '<button class="delete btn">x</button></li>');
+    $('#new-todo').val('');
+  }
+}
+
+function deleteItem() {
+  $(this).parent().remove();
+}
+$(document).ready(function() {
+  $('body').fadeIn(1000);
+  $('#add').on('click', addItem);
+  $(document).on('click', '.delete', deleteItem);
+});
