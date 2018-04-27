6a1bd25677706cca43d03fb16220d57710a2a4c9
Ivan Perun <peruni101@gmail.com>
2018-04-27 16:32:31 -0400

    enter key bind added

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
+// Calls function to add items
+function callItem() {
+  return addItem();
+};
+// Binds enter key
+$('#new-todo').bind("enterKey", function(e) {
+  return addItem();
+});
+
 function addItem() {
-  if ($('input:text').val().length == 0) {
+  if ($('input:text').val().length === 0) {
     $('#todo').effect("shake", {
       distance: 5,
       times: 2
@@ -9,8 +18,8 @@ function addItem() {
     $('#to-do-list').append('<li><input type="checkbox" class="done" required/>' + text + '<button class="delete btn"> X </button></li>');
     $('#new-todo').val('');
   }
-}
-
+};
+// Delete item
 function deleteItem() {
   $(this).parent().remove();
 }
@@ -18,4 +27,10 @@ $(document).ready(function() {
   $('body').fadeIn(1000);
   $('#add').on('click', addItem);
   $(document).on('click', '.delete', deleteItem);
+});
+// Trigger for when Enter is pressed
+$('#new-todo').keyup(function(e) {
+  if (e.keyCode === 13) {
+    $(this).trigger("enterKey");
+  }
 });
